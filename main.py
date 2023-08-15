60af509efa2163251269efb98b40917e39efefe1
宋一凡 <121983035+helloxxsong@users.noreply.github.com>
2023-08-15 19:12:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import openai
 import pandas as pd
 import numpy as np
-import json
 import gradio as gr
 import os
 from dotenv import load_dotenv, find_dotenv
@@ -10,17 +9,45 @@ from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 openai.api_key = os.environ['OPENAI_API_KEY']
 
+# get the directory path of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# build a relative path to TTL_train_data.csv
+csv_file_path = os.path.join(current_dir, 'TTL_train_data.csv')
+
+# read examples from IIT_train_data.csv
+IIT_train_data = pd.read_csv(csv_file_path)
+example = np.array(IIT_train_data['prompt'])
+
 # set delimiter
 delimiter = "####"
 
-# read examples from IIT_train_data.csv
-IIT_train_data = pd.read_csv('./TTI_train_data.csv')
-example = np.array(IIT_train_data['prompt'])
-
-# set train_data for image generate
-train_data = "1"
+# set train_data for text-to-image generate
+train_data = ""
 for item in example:
     train_data += item
     train_data += delimiter
 
-print(train_data)
+
+def button_function(level):
+    return level
+
+
+# create interactive interface with gradio
+with gr.Blocks() as demo:
+    gr.Markdown("# Ink Diffusion")
+    with gr.Row():
+        with gr.Column(scale=4):
+            outputTextbox_1 = gr.Textbox(label="Output", lines=5)
+        with gr.Column(scale=1):
+            button_1 = gr.Button("1")
+            button_2 = gr.Button("2")
+            button_3 = gr.Button("3")
+            button_4 = gr.Button("4")
+            button_refresh = gr.Button("refresh")
+    level = gr.Slider(label="Level", maximum=10, minimum=1, step=1, value=5)
+
+    button_1.click(fn=button_function, inputs=[level], outputs=[outputTextbox_1])
+
+gr.close_all()
+demo.launch()
