c3932aab203c5486266dd23f9158e68639455c0b
宋一凡 <121983035+helloxxsong@users.noreply.github.com>
2023-09-07 10:53:55 +0800

    Update Azure_OpenAI_API.py

--- Azure_OpenAI_API.py
+++ Azure_OpenAI_API.py
@@ -7,7 +7,7 @@ _ = load_dotenv(find_dotenv())
 openai.api_type = "azure"
 openai.api_key = os.environ['AZURE_OPENAI_API_KEY']
 openai.api_base = os.environ['AZURE_OPENAI_API_BASE']
-openai.api_version = "2023-05-15"
+# openai.api_version = "2023-05-15"
 
 
 def get_completion(prompt, temperature=1.0):
@@ -23,11 +23,8 @@ def get_completion(prompt, temperature=1.0):
 def get_completion_from_messages(messages, temperature=1.0):
     response = openai.ChatCompletion.create(
         deployment_id="gpt-35-turbo-16k",
-        model="gpt-35-turbo-16k",   
+        model="gpt-35-turbo-16k",
         messages=messages,
         temperature=temperature
     )
     return response.choices[0].message.content
-
-
-print(get_completion(prompt="Hi"))
